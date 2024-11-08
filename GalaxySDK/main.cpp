4d5521cb17e98b40249f91cfd2108c9716567bc9
drawal <2581478521@qq.com>
2024-11-08 18:04:18 +0800

    获取目标图片

--- GalaxySDK/main.cpp
+++ GalaxySDK/main.cpp
@@ -15,38 +15,43 @@ int main()
         return -1;
     }
 
-    cv::Mat cameraMatrix_L = (cv::Mat_<double>(3, 3) << 105.843, 0.0543, 0, 0, 105.641, 0, 0, 0, 1);
-    cv::Mat cameraMatrix_R = (cv::Mat_<double>(3, 3) << 105.964, 0.0344, 0, 0, 105.737, 0, 0, 0, 1);
+    cv::Mat cameraMatrix = (cv::Mat_<double>(3, 3) << 105.9035, 0.04435, 0, 0, 105.689, 0, 0, 0, 1);
     cv::Mat distCoeffs = (cv::Mat_<double>(1, 5) << 0, 0, 0, 0, 0);
 
     cv::Mat a{};
     std::string window_name = "camera_top";
     cv::namedWindow(window_name, cv::WINDOW_NORMAL);
-    cv::resizeWindow(window_name, cv::Size(640, 480));
+    cv::resizeWindow(window_name, cv::Size(1295, 1024));
 
-    cv::Size imageSize(640,480);
-    cv::Mat newCameraMatrix = cv::getOptimalNewCameraMatrix(cameraMatrix_L, distCoeffs, imageSize, 0);
+    cv::Size imageSize(2592, 2048);
+    cv::Mat newCameraMatrix = cv::getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imageSize, 0);
     cv::Mat map1, map2;
     cv::Mat R = cv::Mat::eye(3, 3, CV_64F);
-    cv::initUndistortRectifyMap(cameraMatrix_L, distCoeffs, R, newCameraMatrix, imageSize, CV_32FC1, map1, map2);
+    cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, R, newCameraMatrix, imageSize, CV_32FC1, map1, map2);
     int count = 0;
     while (camera1.Read(a))
     {
-        int width = a.cols;
-        int height = a.rows;
 
         cv::Mat dst;
         cv::remap(a, dst, map1, map2, cv::INTER_NEAREST);
-        cv::imshow(window_name, a);
+        cv::imshow(window_name, dst);
         if (cv::waitKey(1) == 27)
         {
-            std::cout << width << "  " << height << std::endl;
             break;
         }
-        if (cv::waitKey(1) == 32){
-            std::string filename = "image/clamp_" + std::to_string(count) + ".png";
+        if (cv::waitKey(1) == 32)
+        {
+
+            std::string filename = "E:/WYL_workspace/Visual_Servo_C/GalaxySDK/image/clampddd_" + std::to_string(count) + ".png";
             count++;
-            cv::imwrite(filename, dst);
+
+            if (!cv::imwrite(filename, dst))
+            {
+                std::cerr << "Failed to get picture" << std::endl;
+                break;
+            }
+            else
+                std::cout << "success to save picture " << count << std::endl;
         }
     }
     cv::destroyAllWindows();
